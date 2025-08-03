6eaa0c0473dcbc942b653e905379b8edf6bbc9cb
Tony Faijue <tony.faijue@ndus.edu>
2025-08-03 18:03:21 -0500

    Fixed Responsive Design Issues with BlogSection Page and Improved User Side Nav Color Theme

--- resources/views/components/user/side-nav.blade.php
+++ resources/views/components/user/side-nav.blade.php
@@ -1,27 +1,29 @@
 <!--User Left Sidebar-->
 <div class="">
     <aside class="hidden md:flex flex-col justify-center w-60 h-screen sticky top-0 space-y-3 bg-grad-3">
-        <a href="#" class="flex items-center space-x-3 p-2 rounded hover:bg-teal-200">
+        <a href="#" class="flex items-center space-x-3 p-2 rounded hover:bg-teal-200 hover:text-purple-950">
             <i class="fas fa-user fa-la text-purple-600"></i>
             <span>Profile</span>
         </a>
-        <a href="{{ route('user-blogs-index') }}" class="flex items-center space-x-3 p-2 rounded hover:bg-teal-200">
+        <a href="{{ route('user-blogs-index') }}"
+            class="flex items-center space-x-3 p-2 rounded hover:bg-teal-200  hover:text-purple-950">
             <i class="fas fa-user fa-la text-purple-600"></i>
             <span>My Blogs</span>
         </a>
-        <a href="{{route('user-blogs-create')  }}" class="flex items-center space-x-3 p-2 rounded hover:bg-teal-200">
+        <a href="{{route('user-blogs-create')  }}"
+            class="flex items-center space-x-3 p-2 rounded hover:bg-teal-200  hover:text-purple-950">
             <i class="fas fa-user fa-la text-purple-600"></i>
             <span>Create a Blog</span>
         </a>
-        <a href="#" class="flex items-center space-x-3 p-2 rounded hover:bg-teal-200">
+        <a href="#" class="flex items-center space-x-3 p-2 rounded hover:bg-teal-200  hover:text-purple-950">
             <i class="fas fa-user fa-la text-purple-600"></i>
             <span>My Sections For a Blog</span>
         </a>
-        <a href="#" class="flex items-center space-x-3 p-2 rounded hover:bg-teal-200">
+        <a href="#" class="flex items-center space-x-3 p-2 rounded hover:bg-teal-200  hover:text-purple-950">
             <i class="fas fa-user fa-la text-purple-600"></i>
             <span>Download Blog</span>
         </a>
-        <a href="#" class="flex items-center space-x-3 p-2 rounded hover:bg-teal-200">
+        <a href="#" class="flex items-center space-x-3 p-2 rounded hover:bg-teal-200  hover:text-purple-950">
             <i class="fas fa-user fa-la text-purple-600"></i>
             <span>Profile</span>
         </a>
