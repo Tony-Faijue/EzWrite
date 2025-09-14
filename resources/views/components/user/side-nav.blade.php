fe732956e387343e955be021b30eadc012446f3c
Tony Faijue <tony.faijue@ndus.edu>
2025-09-14 16:45:15 -0500

    Added Links to Blogs, Creating Blog, and About

--- resources/views/components/user/side-nav.blade.php
+++ resources/views/components/user/side-nav.blade.php
@@ -7,25 +7,17 @@
         </a>
         <a href="{{ route('user-blogs-index') }}"
             class="flex items-center space-x-3 p-2 rounded hover:bg-teal-200  hover:text-purple-950">
-            <i class="fas fa-user fa-la text-purple-600"></i>
+            <i class="fa-solid fa-newspaper text-purple-600"></i>
             <span>My Blogs</span>
         </a>
         <a href="{{route('user-blogs-create')  }}"
             class="flex items-center space-x-3 p-2 rounded hover:bg-teal-200  hover:text-purple-950">
-            <i class="fas fa-user fa-la text-purple-600"></i>
+            <i class="fa-solid fa-pencil text-purple-600"></i>
             <span>Create a Blog</span>
         </a>
         <a href="#" class="flex items-center space-x-3 p-2 rounded hover:bg-teal-200  hover:text-purple-950">
-            <i class="fas fa-user fa-la text-purple-600"></i>
-            <span>My Sections For a Blog</span>
-        </a>
-        <a href="#" class="flex items-center space-x-3 p-2 rounded hover:bg-teal-200  hover:text-purple-950">
-            <i class="fas fa-user fa-la text-purple-600"></i>
+            <i class="fa-solid fa-print text-purple-600"></i>
             <span>Download Blog</span>
-        </a>
-        <a href="#" class="flex items-center space-x-3 p-2 rounded hover:bg-teal-200  hover:text-purple-950">
-            <i class="fas fa-user fa-la text-purple-600"></i>
-            <span>Profile</span>
         </a>
     </aside>
 </div>
